786f7a2273358365887eb2e9a7fc7c6219a11553
Vizion <88829701+TwizViz@users.noreply.github.com>
2022-08-08 19:49:03 -0400

    1. set up game item in front end

--- the_icebox/src/components/games/GameItem.js
+++ the_icebox/src/components/games/GameItem.js
@@ -11,39 +11,41 @@ import {
 } from '@mui/material';
 import { games } from '../../helpers/gameItems';
 
-const CardItem = () => {
+const GameCardItem = () => {
     return(
         <div className='game-card-item'>
-            {games.map((game) => (
-                <Card key={game.id} sx={{ ml: 2, width: 362.5, height: 400, border: 'solid 1px black', background: 'orchid' }}>
-                    <Grid item>
-                        <Typography sx={{ fontFamily: "'Press Start 2P', cursive", fontSize: '16px', fontWeight: 700, color: 'yellow', pt: 2 }}>
-                            {game.title}
-                        </Typography>
-                    </Grid>
-                    <CardContent sx={{ height: 330, width: 330, display: 'flex', justifyContent: 'center' }}>
-                        <Stack direction='column'>
-                            <Grid item>
-                                <Paper elevation={0} variant='outlined' sx={{ height: 295, width: 353, backgroundColor: 'orange', border: 'solid 5px' }}>
-                                    <img src={game.img} alt='thumb' sx={{ border: 'solid 1px black' }} />
-                                </Paper>
-                            </Grid>
-                            <Grid item>
-                                <Stack direction='row' sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <Box sx={{ borderRight: 'solid', width: '50%', mt: 0.25 }}>
-                                        <Button type='button' variant='text' sx={{ color: 'yellow', fontWeight: 700, fontFamily: "'Press Start 2P', cursive" }}>More</Button>
-                                    </Box>
-                                    <Box sx={{ borderLeft: 'solid', width: '50%', mt: 0.25 }}>
-                                        <Button type='button' variant='text' sx={{ color: 'yellow', fontWeight: 700, fontFamily: "'Press Start 2P', cursive" }}>Play</Button>
-                                    </Box>
-                                </Stack>
-                            </Grid>
-                        </Stack>
-                    </CardContent>
-                </Card>
-            ))}
+            <Stack direction='row' spacing={2} sx={{ mt: 1, justifyContent: 'center' }}>
+                {games.map((game) => (
+                    <Card key={game.id} sx={{ width: 240, height: 282, border: 'solid 1px black', background: 'orchid' }}>
+                        <Grid item>
+                            <Typography sx={{ fontFamily: "'Press Start 2P', cursive", fontSize: '16px', fontWeight: 700, color: 'yellow', pt: 2 }}>
+                                {game.title}
+                            </Typography>
+                        </Grid>
+                        <CardContent sx={{ height: 220, width: 220, display: 'flex', justifyContent: 'center' }}>
+                            <Stack direction='column'>
+                                <Grid item>
+                                    <Paper elevation={0} variant='outlined' sx={{ height: 175, width: 230, backgroundColor: 'orange', border: 'solid 5px', mr: 1.5 }}>
+                                        <img src={game.img} alt='thumb' />
+                                    </Paper>
+                                </Grid>
+                                <Grid item>
+                                    <Stack direction='row' sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                                        <Box sx={{ borderRight: 'solid', width: '50%', mt: 0.25 }}>
+                                            <Button type='button' variant='text' sx={{ color: 'yellow', fontWeight: 700, fontFamily: "'Press Start 2P', cursive" }}>More</Button>
+                                        </Box>
+                                        <Box sx={{ borderLeft: 'solid', width: '50%', mt: 0.25 }}>
+                                            <Button type='button' variant='text' sx={{ color: 'yellow', fontWeight: 700, fontFamily: "'Press Start 2P', cursive" }}>Play</Button>
+                                        </Box>
+                                    </Stack>
+                                </Grid>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                ))}
+            </Stack>
         </div>
     );
 }
 
-export default CardItem;
+export default GameCardItem;
--- the_icebox/src/pages/GamesPage.js
+++ the_icebox/src/pages/GamesPage.js
@@ -1,56 +1,18 @@
 import React from 'react';
 import { 
-    Stack,
-    Typography,
-    Button,
-    Card,
-    CardContent,
-    Divider,
-    Paper,
     Box,
-    Grid
+    Typography
 } from '@mui/material';
-import { games } from '../helpers/gameItems';
+import GameCardItem from '../components/games/GameItem';
 
 
 const GamesPage = () => {
     return(
         <div className='login-page' sx={{ m: 0, p: 0 }}>
-            <Typography component='h1' variant='h3' sx={{ fontFamily: "'Press Start 2P', cursive" }}>Games</Typography>
-                {games.map((game) => (
-                    <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'space-around' }}>
-                        <Grid container direction='column' sx={{ justifyContent: 'center' }}>
-                            <Stack direction='row' spacing={0}>
-                                <Card key={game.id} sx={{ ml: 2, width: 362.5, height: 400, border: 'solid 1px black', background: 'orchid' }}>
-                                    <Grid item>
-                                        <Typography sx={{ fontFamily: "'Press Start 2P', cursive", fontSize: '16px', fontWeight: 700, color: 'yellow', pt: 2 }}>
-                                            {game.title}
-                                        </Typography>
-                                    </Grid>
-                                    <CardContent sx={{ height: 330, width: 330, display: 'flex', justifyContent: 'center' }}>
-                                        <Stack direction='column'>
-                                            <Grid item>
-                                                <Paper elevation={0} variant='outlined' sx={{ height: 295, width: 353, backgroundColor: 'orange', border: 'solid 5px' }}>
-                                                    <img src={game.img} alt='thumb' sx={{ border: 'solid 1px black' }} />
-                                                </Paper>
-                                            </Grid>
-                                            <Grid item>
-                                                <Stack direction='row' sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                                                    <Box sx={{ borderRight: 'solid', width: '50%', mt: 0.25 }}>
-                                                        <Button type='button' variant='text' sx={{ color: 'yellow', fontWeight: 700, fontFamily: "'Press Start 2P', cursive" }}>More</Button>
-                                                    </Box>
-                                                    <Box sx={{ borderLeft: 'solid', width: '50%', mt: 0.25 }}>
-                                                        <Button type='button' variant='text' sx={{ color: 'yellow', fontWeight: 700, fontFamily: "'Press Start 2P', cursive" }}>Play</Button>
-                                                    </Box>
-                                                </Stack>
-                                            </Grid>
-                                        </Stack>
-                                    </CardContent>
-                                </Card>
-                            </Stack>
-                        </Grid>
-                    </Box>
-                ))}
+            <Box sx={{ backgroundColor: 'gray', width: '100%', height: '100vh' }}>
+                <Typography component='h1' variant='h3' sx={{ fontFamily: "'Press Start 2P', cursive", pt: 3, pb: 3 }}>Games</Typography>
+                <GameCardItem />
+            </Box>
         </div>
     );
 }
